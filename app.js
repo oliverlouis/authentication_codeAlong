fd39bdc359271f8abe30f05f1495bce9591855ae
Oliver Louis <oliverlouis@me.com>
2020-06-03 11:52:45 +0200

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	User = require('./models/user'),
+	LocalStrategy = require('passport-local'),
+	passport = require('passport'),
+	passportLocalMongoose = require('passport-local-mongoose');
 
 mongoose
 	.connect('mongodb://localhost/Auth_demo', {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => console.log('Connected to Database!!'))
 	.catch((err) => console.log("Couldn't connect to database", err));
 
+app.use(
+	require('express-session')({
+		secret: 'I love Arielle so much',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
 	res.render('home');
